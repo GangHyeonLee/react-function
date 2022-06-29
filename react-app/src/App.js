70cde690ac1431faecb7ccdb8a627d02c5eeda38
GangHyeonLee <isaac11191@gmail.com>
2022-06-29 13:38:59 +0900

    modify App.js state

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,25 +1,33 @@
 //import logo from './logo.svg';
-// import './App.css';
+import './App.css';
 import React,{ Component } from 'react'
-// import TOC from './components/TOC'  // ./ 이게 현재 위치라는 뜻임
-// import Subject from './components/Subject'
-// import Contents from './components/Contents'
+import TOC from './components/TOC'  // ./ 이게 현재 위치라는 뜻임
+import Subject from './components/Subject'
+import Contents from './components/Contents'
 
 // import Header from './test_homepage/Header'
 // import Content from './test_homepage/Content';
 // import Footer from './test_homepage/Footer';
 
 class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      subject:{title: 'WEB', sub:'world wid web'},
+      contents:[
+        {id:1, title:'HTML',desc:'HTML is for imformation'},
+        {id:2, title:'CSS',desc:'CSS is for design'},
+        {id:3, title:'JavaScript',desc:'JavaScript is for interactive'},
+      ]
+    }
+  }
   render(){
   return (
-    // <div>
-    //   <Subject title="WEB" sub="world wide web!"></Subject>  {/* <Subject/> */}   
-    //   <TOC></TOC>
-    //   <Contents title="HTML" sub="HTML is HyperText Markup Language."></Contents>
-    // </div>
-    <div>
-      
-    </div>
+     <div>
+       <Subject title={this.state.subject.title} sub={this.state.subject.sub}></Subject>  {/* <Subject/> */}   
+       <TOC data={this.state.contents}></TOC>
+       <Contents title="HTML" sub="HTML is HyperText Markup Language."></Contents>
+     </div>
   );
 
 
